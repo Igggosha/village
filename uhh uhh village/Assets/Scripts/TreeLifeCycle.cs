261e59fbbedd005e267d92e6600412e0cb8c8198
Igggosha <igggoshastudios@gmail.com>
2023-07-05 18:46:37 +0100

    Apples
    apple tree system
    im lazy

--- uhh uhh village/Assets/Scripts/TreeLifeCycle.cs
+++ uhh uhh village/Assets/Scripts/TreeLifeCycle.cs
@@ -5,11 +5,25 @@ using UnityEngine;
 public class TreeLifeCycle : MonoBehaviour
 {
     //[SerializeField] private int MaxLifeTime;
-    [SerializeField] private int MaxSize = 5;
+    [SerializeField] private float MaxSize;
+    [SerializeField] private bool isAppleTree = false;
+    [SerializeField] private bool isFlourishing = false; // worst name i could think off for this but this is to indicate if an apple tree has apples already
 
+    [SerializeField] private Material transparent;
+    [SerializeField] private Material applered;
+    [SerializeField] private float timeModifier = 10f;
     // Start is called before the first frame update
     void Start()
     {
+        MaxSize =  Random.Range(0.5f, 5f);
+        for (var i = gameObject.transform.childCount - 1; i >= 0; i--)
+        {
+            if (gameObject.transform.GetChild(i).gameObject.name == "apples")
+            {
+                isAppleTree = true;
+                break;
+            }
+        }
         
     }
 
@@ -19,27 +33,33 @@ public class TreeLifeCycle : MonoBehaviour
         if (gameObject.transform.localScale.x <= MaxSize)
         {
             gameObject.transform.localScale = new Vector3(
-                gameObject.transform.localScale.x + Time.deltaTime / 10,
-                gameObject.transform.localScale.y + Time.deltaTime / 10,
-                gameObject.transform.localScale.z + Time.deltaTime / 10);
+                gameObject.transform.localScale.x + Time.deltaTime / timeModifier,
+                gameObject.transform.localScale.y + Time.deltaTime / timeModifier,
+                gameObject.transform.localScale.z + Time.deltaTime / timeModifier);
+
+            if (isAppleTree && !isFlourishing && gameObject.transform.localScale.x >= 1)
+            {
+                StartCoroutine(appleCountdown(Random.Range(3f, 10f)));
+            }
         } else
         {
-            StartCoroutine(deathCountdown());
+            StartCoroutine(deathCountdown(Random.Range(20f, 60f)));
         }
 
     }
 
-    private IEnumerator deathCountdown ()
+    private IEnumerator deathCountdown (float staletime)
     {
-        yield return new WaitForSeconds(20);
+        yield return new WaitForSeconds(staletime);
 
         for (var i = gameObject.transform.childCount - 1; i >= 0; i--)
         {
-            if (gameObject.transform.GetChild(i).gameObject.name == "Icosphere")
+            if (gameObject.transform.GetChild(i).gameObject.name == "Icosphere" || gameObject.transform.GetChild(i).gameObject.name == "apples")
             {
                 Destroy(gameObject.transform.GetChild(i).gameObject);
-            }
+            } 
         }
+
 
         StartCoroutine(rotCountdown());
     }
@@ -49,5 +69,35 @@ public class TreeLifeCycle : MonoBehaviour
         yield return new WaitForSeconds(10);
 
         Destroy(gameObject);
+    }
+
+    private IEnumerator appleCountdown(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+
+
+        for (var i = gameObject.transform.childCount - 1; i >= 0; i--)
+        {
+            if (gameObject.transform.GetChild(i).gameObject.name == "apples")
+            {
+                gameObject.transform.GetChild(i).gameObject.GetComponent<MeshRenderer>().material = applered;
+                break;
+            }
+        }
+        
+        isFlourishing = true;
+    }
+
+    private void deflourish()
+    {
+        for (var i = gameObject.transform.childCount - 1; i >= 0; i--)
+        {
+            if (gameObject.transform.GetChild(i).gameObject.name == "apples")
+            {
+                gameObject.transform.GetChild(i).gameObject.GetComponent<MeshRenderer>().material = transparent;
+                break;
+            }
+        }
+        isFlourishing = false;
     }
 }
