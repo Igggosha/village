d7b595dd6c3e65b2bf340053a9e571c39e3ebd09
Igggosha <igggoshastudios@gmail.com>
2023-07-09 18:31:59 +0100

    Further villager AI work
    Further built structure for the AI, almost full implementation of priority.
    gtg can't make a full doc

--- uhh uhh village/Assets/Scripts/VillagerBehaviour.cs
+++ uhh uhh village/Assets/Scripts/VillagerBehaviour.cs
@@ -111,6 +111,25 @@ public class VillagerBehaviour : MonoBehaviour
         {
             agent.SetDestination(target.position);
         }
+
+        switch (priority)
+        {
+            case 0:
+                WorkOnGettingGroupFood();
+                break;
+            case 1:
+                WorkOnGettingShelter();
+                break;
+            case 2:
+                WorkOnGettingPrivateFood();
+                break;
+            case 3:
+                WorkOnGettingMate();
+                break;
+            case 4:
+                WorkOnGettingExploration();
+                break;
+        }
     }
 
     private void OnMouseDown()
@@ -163,13 +182,81 @@ public class VillagerBehaviour : MonoBehaviour
         foreach (GameObject go in gos)
         {
             Vector3 diff = go.transform.position - position;
-            float curDistance = diff.sqrMagnitude;
-            if (curDistance < distance)
+            float currentDistance = diff.sqrMagnitude;
+            if (currentDistance < distance)
             {
                 closest = go;
-                distance = curDistance;
+                distance = currentDistance;
             }
         }
         return closest;
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.transform == target)
+        {
+            target = null;
+        }
+    }
+
+
+    // ============================================== //
+    // ====== VILLAGER PRIORITY-CALLED ACTIONS ====== //
+    // ============================================== //
+
+    /// <summary>
+    /// This function is going to describe how the villager should behave and what
+    /// they should do in order to get food for their group/village
+    /// !!!!!! :construction: nothing here yet!!!!!!
+    /// </summary>
+    private void WorkOnGettingGroupFood()
+    {
+
+    }
+
+
+    /// <summary>
+    /// This function is going to describe how the villager should behave and what
+    /// they should do in order to get shelter
+    /// !!!!!! :construction: nothing here yet!!!!!!
+    /// </summary>
+    private void WorkOnGettingShelter ()
+    {
+
+    }
+
+
+    /// <summary>
+    /// This function is going to describe how the villager should behave and what
+    /// they should do in order to get food for themselves
+    /// !!!!!! :construction: nothing here yet!!!!!!
+    /// </summary>
+    private void WorkOnGettingPrivateFood()
+    {
+
+    }
+
+
+    /// <summary>
+    /// This function is going to describe how the villager should behave and what
+    /// they should do in order to mate and procreate.
+    /// !!!!!! :construction: nothing here yet!!!!!!
+    /// </summary>
+    private void WorkOnGettingMate()
+    {
+
+    }
+
+
+    /// <summary>
+    /// This function is going to describe how the villager should behave and what
+    /// they should do in order to expllore the world and learn new technology for their village.
+    /// !!!!!! :construction: nothing here yet!!!!!!
+    /// </summary>
+    private void WorkOnGettingExploration()
+    {
+
     }
 }
